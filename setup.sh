a658ce67333240b1537d088d389b5d629d2cab69
Socio-path <doohee006@gmail.com>
2019-10-26 19:07:51 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
 GOPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+go get -u -v github.com/asticode/go-astilectron
+go get -u -v github.com/asticode/go-astilectron-bootstrap
+go get -u -v github.com/asticode/go-astilectron-bundler/...
+go get -u -v github.com/asticode/go-astilog
