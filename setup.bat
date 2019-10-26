9108b8040d5c8037d4cbe96b3846a495cd0ad87a
Socio-path <doohee006@gmail.com>
2019-10-26 18:30:36 +0900

    Create setup.bat

---
+++ setup.bat
@@ -0,0 +1,7 @@
+@echo off
+set GOPATH=%cd%
+go get -u -v github.com/asticode/go-astilectron
+go get -u -v github.com/asticode/go-astilectron-bootstrap
+go get -u -v github.com/asticode/go-astilectron-bundler
+go get -u -v github.com/asticode/go-astilog
+
