be1600d4258dcee2bfd76cacb7f95198bf4dbb04
Socio-path <doohee006@gmail.com>
2019-10-29 20:35:03 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,4 +7,5 @@ go get -u -v github.com/asticode/go-astilectron-bundler/...
 go get -u -v github.com/asticode/go-astilog
 go get -u -v github.com/asticode/go-astichartjs
 go get -u -v github.com/mattn/go-colorable
+go get -u -v github.com/konsorten/go-windows-terminal-sequences
 go install github.com/asticode/go-astilectron-bundler/astilectron-bundler
