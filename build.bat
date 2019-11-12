1d84a3065312bb131166e9868796a97abf4137bb
Socio-path <doohee006@gmail.com>
2019-11-12 13:37:20 +0900

    Changing UI Engine to Qt

--- build.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set GOPATH=%cd%
-bin\astilectron-bundler.exe -v
-
--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-export GOPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-bin/astilectron-bundler -v
