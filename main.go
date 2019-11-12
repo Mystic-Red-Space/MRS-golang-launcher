71195d6760ca53e751b72b4d6339b80064cb8739
Socio-path <doohee006@gmail.com>
2019-11-12 22:42:08 +0900

    Add MainWindow

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/therecipe/qt/widgets"
+	"os"
+)
+
+func main() {
+	app := widgets.NewQApplication(len(os.Args), os.Args)
+
+	window := widgets.NewQMainWindow(nil, 0)
+	window.SetMinimumSize2(640, 320)
+	window.SetWindowTitle("MRS Launcher")
+
+	widget := widgets.NewQWidget(window, 0)
+	widget.SetLayout(widgets.NewQVBoxLayout())
+	window.SetCentralWidget(widget)
+
+	idInput := widgets.NewQLineEdit(widget)
+	idInput.SetPlaceholderText("E-mail")
+	idInput.SetEchoMode(widgets.QLineEdit__PasswordEchoOnEdit)
+	widget.Layout().AddChildLayout(idInput)
+	window.Show()
+	app.Exec()
+
+}
